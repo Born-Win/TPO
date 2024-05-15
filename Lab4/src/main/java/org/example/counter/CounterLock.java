1362bd59271a40374704c59ada64d245e287b46b
Born-Win <g.l.e.b.b.a.s.66@gmail.com>
2024-05-15 21:01:58 +0200

    Implemented Lab4

---
+++ Lab4/src/main/java/org/example/counter/CounterLock.java
@@ -0,0 +1,39 @@
+package org.example.counter;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+public class CounterLock implements ICounter  {
+    private int value = 0;
+    private Lock locker = new ReentrantLock();
+
+    @Override
+    public int getValue() {
+        return this.value;
+    }
+
+    @Override
+    public void increment() {
+        try {
+            locker.lock();
+            this.value++;
+        } finally {
+            locker.unlock();
+        }
+    }
+
+    @Override
+    public void decrement() {
+        try {
+            locker.lock();
+            this.value--;
+        } finally {
+            locker.unlock();
+        }
+    }
+
+    @Override
+    public void resetCounter() {
+        this.value = 0;
+    }
+}
+
---
+++ Lab4/src/main/java/org/example/counter/ICounter.java
@@ -0,0 +1,9 @@
+package org.example.counter;
+
+public interface ICounter {
+    int getValue();
+    void increment();
+    void decrement();
+    void resetCounter();
+}
+
